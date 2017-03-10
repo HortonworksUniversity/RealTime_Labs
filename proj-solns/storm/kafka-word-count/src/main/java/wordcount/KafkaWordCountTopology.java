ce89d9638a6ae9b7f2465ed1be620e841a980619
Lester Martin <lester.martin@gmail.com>
2017-03-10 14:41:17 -0500

    changes for kafka adaptation

---
+++ proj-solns/storm/kafka-word-count/src/main/java/wordcount/KafkaWordCountTopology.java
@@ -0,0 +1,55 @@
+package wordcount;
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.generated.InvalidTopologyException;
+import org.apache.storm.kafka.*;
+import org.apache.storm.spout.SchemeAsMultiScheme;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
+
+import java.util.UUID;
+
+public class KafkaWordCountTopology {
+
+    public static void main(String[] args) throws InvalidTopologyException, AuthorizationException, AlreadyAliveException {
+        TopologyBuilder builder = new TopologyBuilder();
+
+        BrokerHosts hosts = new ZkHosts("zk1:2181,zk2:2181,zk3:2181");
+
+        SpoutConfig sc = new SpoutConfig(hosts,
+                "s20-sentences", "/s20-sentences",
+                UUID.randomUUID().toString());
+        sc.scheme = new SchemeAsMultiScheme(new StringScheme());
+
+        KafkaSpout spout = new KafkaSpout(sc);
+
+        builder.setSpout("kafka-spout", spout, 1);
+
+        builder.setBolt("splitter",
+                new SplitSentenceBolt(), 1)
+                .shuffleGrouping("kafka-spout");
+
+        builder.setBolt("counter",
+                new WordCountBolt(), 1)
+                .fieldsGrouping("splitter", new Fields("word"));
+
+
+        Config conf = new Config();
+        conf.setDebug(true);
+        conf.setNumWorkers(1);
+
+        /*
+        StormSubmitter.submitTopologyWithProgressBar(
+                "word-count", conf,
+                builder.createTopology());
+        */
+
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("word-count-local",
+                conf, builder.createTopology());
+
+    }
+}
--- proj-solns/storm/kafka-word-count/src/main/java/wordcount/SplitSentenceBolt.java
+++ proj-solns/storm/kafka-word-count/src/main/java/wordcount/SplitSentenceBolt.java
@@ -12,7 +12,7 @@ import org.apache.storm.tuple.Values;
 public class SplitSentenceBolt extends BaseBasicBolt {
 
     public void execute(Tuple tuple, BasicOutputCollector basicOutputCollector) {
-        String[] words = StringUtils.split(tuple.getStringByField("sentence"));
+        String[] words = StringUtils.split(tuple.getString(0));
         for( String word : words ) {
             System.out.println("\n*** Split Sentence Bolt *** " + word + " ***\n");
             basicOutputCollector.emit(new Values(word));
