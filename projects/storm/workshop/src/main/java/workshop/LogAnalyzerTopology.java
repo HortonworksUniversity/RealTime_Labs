03ca88c223e927e11d6e776d6dcd2bbfb5c62af4
Lester Martin <lester.martin@gmail.com>
2017-03-11 00:47:22 -0500

    gutted it for students to complete the topology

--- projects/storm/workshop/src/main/java/workshop/LogAnalyzerTopology.java
+++ projects/storm/workshop/src/main/java/workshop/LogAnalyzerTopology.java
@@ -22,38 +22,6 @@ public class LogAnalyzerTopology {
     public static void main(String[] args) throws InvalidTopologyException, AuthorizationException, AlreadyAliveException {
         TopologyBuilder builder = new TopologyBuilder();
 
-        BrokerHosts hosts = new ZkHosts("zk1:2181,zk2:2181,zk3:2181");
-
-        SpoutConfig sc = new SpoutConfig(hosts,
-                "s20-logs", "/s20-logs",
-                UUID.randomUUID().toString());
-        sc.scheme = new SchemeAsMultiScheme(new StringScheme());
-
-        KafkaSpout spout = new KafkaSpout(sc);
-
-        builder.setSpout("log-spout", spout, 1);
-
-        builder.setBolt("message-tokenizer",
-                new MessageTokenizerBolt(), 1)
-                .shuffleGrouping("log-spout");
-
-        builder.setBolt("message-filterer",
-                new MessageFiltererBolt(), 1)
-                .shuffleGrouping("message-tokenizer");
-
-
-        SimpleHBaseMapper mapper = new SimpleHBaseMapper()
-                .withRowKeyField("ip-address")
-                .withColumnFields(new Fields("type", "details"))
-                .withCounterFields(new Fields("total"))
-                .withColumnFamily("event");
-
-        HBaseBolt hbase = new HBaseBolt("s20_incident", mapper)
-                .withConfigKey("hbase.config");
-
-        builder.setBolt("hbase-bolt", hbase, 1)
-                .shuffleGrouping("message-filterer");
-
         Config conf = new Config();
         conf.setDebug(true);
         conf.setNumWorkers(1);
@@ -64,14 +32,15 @@ public class LogAnalyzerTopology {
         conf.put("hbase.config", mapHbase);
 
 
+        /*
         StormSubmitter.submitTopologyWithProgressBar(
                 "log-analyzer", conf,
                 builder.createTopology());
-
-        /*
+        */
+        
         LocalCluster cluster = new LocalCluster();
         cluster.submitTopology("log-analyzer-local",
                 conf, builder.createTopology());
-        */
+        
     }
 }
